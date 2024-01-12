b93c6d67113653eaa007c241988d55c3518bb1d8
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2024-01-12 10:26:43 +0100

    azure

--- main.go
+++ main.go
@@ -4,17 +4,16 @@ import (
 	"context"
 	"fmt"
 	"github.com/adrienpessu/go-lib-example/github"
+	"os"
 )
 
 func main() {
 	// Do nothing
 	fmt.Println("Hello World!")
 
-	aws_access_key_id := "AKIA2OGYBAH6W4T5GLFN"
-	aws_secret_access_key := "qDLsYCzdMR7PSZxXFKfyKB7yLW2dY8P2nsY0Rjfj"
+	azureStorageKey := os.Getenv("AZURE_STORAGE_KEY")
 
-	fmt.Println(aws_access_key_id)
-	fmt.Println(aws_secret_access_key)
+	fmt.Println(azureStorageKey)
 
 	// Call the function from the lib
 	client := github.NewClient(nil)
