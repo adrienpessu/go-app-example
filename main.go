1a88984d5824b88d0a698a1a0b251c47780e1cd9
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2024-12-05 06:09:43 +0100

    new feature

--- main.go
+++ main.go
@@ -4,25 +4,56 @@ import (
 	"context"
 	"fmt"
 	"github.com/adrienpessu/go-lib-example/github"
-	"os"
+	"golang.org/x/crypto/ssh"
+	"math/rand"
+	"net"
 )
+
+var charset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 func main() {
 	// Do nothing
 	fmt.Println("Hello World!")
 
-	azureStorageKey := os.Getenv("AZURE_STORAGE_KEY")
-
-	fmt.Println(azureStorageKey)
-
 	// Call the function from the lib
 	client := github.NewClient(nil)
 
-	// list all organizations for user "willnorris"
+	// list all organizations for user "adrienpessu"
 	orgs, _, err := client.Organizations.List(context.Background(), "adrienpessu", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println(orgs)
+	fmt.Println(generatePassword())
+
+	insecureIgnoreHostKey()
+	insecureHostKeyCallback()
+}
+
+func generatePassword() string {
+	s := make([]rune, 20)
+	for i := range s {
+		s[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(s)
+}
+
+func insecureIgnoreHostKey() {
+	_ = &ssh.ClientConfig{
+		User:            "username",
+		Auth:            []ssh.AuthMethod{nil},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+}
+
+func insecureHostKeyCallback() {
+	_ = &ssh.ClientConfig{
+		User: "username",
+		Auth: []ssh.AuthMethod{nil},
+		HostKeyCallback: ssh.HostKeyCallback(
+			func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+				return nil
+			}),
+	}
 }
